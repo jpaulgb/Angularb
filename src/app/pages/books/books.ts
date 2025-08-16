cae0af1453813c88001f05ecf1b9f103d612b7b6
jpaulgb <jepgb@hotmail.com>
2025-08-16 03:41:48 -0500

    fetch de libros

--- src/app/pages/books/books.ts
+++ src/app/pages/books/books.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-books',
-  imports: [],
+  standalone: true, // standalone
+  imports: [CommonModule],
   templateUrl: './books.html',
   styleUrl: './books.css'
 })
-export class Books {
+export class Books implements OnInit {
 
+  libros: any[] = [];
+
+  async ngOnInit() {
+    await this.getLibros();
+  }
+
+  async getLibros() {
+    try {
+      const response = await fetch('http://localhost:3000/api/libros');
+      const data = await response.json();
+      this.libros = data;
+    } catch (error) {
+      console.error('Error al obtener libros', error);
+    }
+  }
 }
